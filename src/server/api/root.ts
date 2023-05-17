e0776303020f002b6cf1e22dd88f9b0985bafd66
yago-quintino <yagoquintino21@gmail.com>
2023-05-17 17:39:30 -0300

    Add note api router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -11,6 +11,7 @@ import { noteRouter } from "./routers/note";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
