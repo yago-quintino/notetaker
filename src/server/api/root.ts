0d2144fbcef39c5fcd77edc9385274f0341132eb
yago-quintino <yagoquintino21@gmail.com>
2023-05-17 17:38:17 -0300

    Add topic api router and basic topics display for index page

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { topicRouter } from "./routers/topic";
+import { noteRouter } from "./routers/note";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topic: topicRouter,
 });
 
 // export type definition of API
